3c9692bf7790b1709c0bb21b543eac96567556f7
Leo Bergqvist <leo@bergqvist.se>
2022-10-28 14:47:43 +0200

    Added websites + get email
    Added nordace.
    Added kohls.
    Added banana republic.
    Added target.
    Added input for getting email and checks if it's a valid email-adress.

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-ssl-errors')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 spam_sites = [
     'https://www.scientificamerican.com/page/newsletter-sign-up/?origincode=2019_sciam_StayInformed_NewsletterSignUp',
     'https://www.scientology.org/daily-connect/?subscribe=1',
+    'https://nordace.com/en/',
+    'https://www.kohls.com/homepage/sale_alert_signup.jsp',
+    'https://www.bananarepublic.co.uk/interstitial/UK/BananaRepublic/index.html',
+    'https://www.target.com/',
 ]
-    
 
-def get_email():
-    return str(input("What's your email?"))
+
+_email = ''
+
+email_check = True
+while email_check == True:
+    _email = str(input("What's your email?"))
+
+    if '@' in _email and '.' in _email:
+        email_check = False
 
 def spammer():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
-    _email = 'example@gmail.com'
-
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=options)
     for url in spam_sites:
         driver.get(url)
 
@@ -37,13 +50,50 @@ def spammer():
         
         if url == 'https://www.scientology.org/daily-connect/?subscribe=1':
             try:
-                driver.find_element("xpath", "//input[@name='firstName']").send_keys('Miro')
+                driver.find_element("xpath", "//input[@name='firstName']").send_keys('name')
                 driver.find_element("xpath", "//input[@name='email']").send_keys(_email)
                 driver.find_element("xpath", "//button[@type='submit']").click()
                 
             except Exception as e:
                 print(e)
                 print('Scientology failed')
+
+        if url == 'https://nordace.com/en/':
+            try:
+                driver.find_element("xpath", "//input[@name='input_1']").send_keys(_email)
+                driver.find_element("xpath", "//input[@type='submit']").click()
+
+            except Exception as e:
+                print(e)
+                print('Nordace failed')
+
+        if url == 'https://www.kohls.com/homepage/sale_alert_signup.jsp':
+            try:
+                driver.find_element("xpath", "//input[@name='enterEmail']").send_keys(_email)    
+                driver.find_element("xpath", "//input[@name='emailConfirm']").send_keys(_email)    
+                driver.find_element("xpath", "//button[@class='addtolist']").click()
+
+            except Exception as e:
+                print(e)
+                print('Kohls failed')
+
+        if url == 'https://www.bananarepublic.co.uk/interstitial/UK/BananaRepublic/index.html':
+            try:
+                driver.find_element("xpath", "//input[@name='email']").send_keys(_email)    
+                driver.find_element("xpath", "//input[@class='sds_absolute']").click()
+
+            except Exception as e:
+                print(e)
+                print('Banana republic failed')
+
+        if url == 'https://www.target.com/':
+            try:
+                driver.find_element("xpath", "//input[@name='email-address']").send_keys(_email)    
+                driver.find_element("xpath", "//button[@id='submit-button']").click()
+
+            except Exception as e:
+                print(e)
+                print('Target failed')
 
     time.sleep(5)
     driver.quit()
